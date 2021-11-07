615507ab761d9fc83947415eb369706e0c205d20
ashokkumar2342 <ashokkumar2342@gmail.com>
2021-11-07 09:51:04 +0530

    Update booking.blade.php

--- resources/views/admin/booking/booking.blade.php
+++ resources/views/admin/booking/booking.blade.php
@@ -19,7 +19,7 @@
                         <div class="col-lg-6 form-group"> 
                             <label for="exampleInputEmail1">Booking Type</label>
                             <span class="fa fa-asterisk"></span>
-                            <select class="form-control" name="booking_type">
+                            <select class="form-control" name="booking_type" required>
                             <option selected disabled>Select Booking Type</option>
                             @foreach ($bookingTypes as $bookingType)
                                 <option value="{{$bookingType->id}}">{{$bookingType->code}}-{{$bookingType->name}}</option>
@@ -30,39 +30,39 @@
                         <div class="col-lg-6 form-group">
                             <label>Booking Date</label>
                             <span class="fa fa-asterisk"></span> 
-                            <input type="date" name="booking_date" class="form-control" id="exampleInputEmail1" maxlength="50" min="{{ date('Y-m-d',strtotime(date('d-m-Y'))) }}"> 
+                            <input type="date" name="booking_date" class="form-control" id="exampleInputEmail1" maxlength="50" min="{{ date('Y-m-d',strtotime(date('d-m-Y'))) }}" required> 
                         <p class="text-danger">{{ $errors->first('booking_date') }}</p> 
                         </div>
                         <div class="col-lg-3 form-group">
                             <label>Adults</label>
                             <span class="fa fa-asterisk"></span> 
-                            <input type="text" name="adults" class="form-control section" id="adult_div" maxlength="3" onkeypress='return event.charCode >= 48 && event.charCode <= 57;amontAdd()'> 
+                            <input type="text" name="adults" class="form-control section" id="adult_div" maxlength="3" onkeyup="amontAdd()" value="0" required> 
                         <p class="text-danger">{{ $errors->first('adults') }}</p> 
                         </div>
                         <div class="col-lg-3 form-group">
                             <label>Children</label>
                             <span class="fa fa-asterisk"></span> 
-                            <input type="text" name="children" class="form-control section" id="children_div" maxlength="3" onkeypress='return event.charCode >= 48 && event.charCode <= 57'> 
+                            <input type="text" name="children" class="form-control section" id="children_div" maxlength="3" onkeyup="amontAdd()" value="0"> 
                         <p class="text-danger">{{ $errors->first('children') }}</p>
                         </div>
                         <div class="col-lg-3 form-group">
                             <label>Team Leader Name</label>
                             <span class="fa fa-asterisk"></span> 
-                            <input type="text" name="team_leader_name" class="form-control" id="exampleInputEmail1" maxlength="50"> 
+                            <input type="text" name="team_leader_name" class="form-control" id="exampleInputEmail1" maxlength="50" required> 
                         <p class="text-danger">{{ $errors->first('team_leader_name') }}</p>
                         </div>
                         <div class="col-lg-3 form-group">
                             <label>Leader Mobile No.</label>
                             <span class="fa fa-asterisk"></span> 
-                            <input type="text" name="leader_mobile_no" class="form-control" id="exampleInputEmail1"maxlength="10" onkeypress='return event.charCode >= 48 && event.charCode <= 57'> 
+                            <input type="text" name="leader_mobile_no" class="form-control" id="exampleInputEmail1"maxlength="10" onkeypress='return event.charCode >= 48 && event.charCode <= 57' required> 
                         <p class="text-danger">{{ $errors->first('leader_mobile_no') }}</p> 
                         </div>
                         <div class="col-lg-4 form-group" style="margin-top: 30px"> 
                             <div class="card bg-gray"> 
                               <div class="form-group clearfix">
                                 <div class="icheck-primary d-inline"> 
-                                  <input type="hidden" name="total_amount" class="hidden" value="1">
-                                  <span style="margin: 15px"> <b>Total Amount : 1</b></span>
+                                  <input type="text" name="total_amount"  id="total_amount_hidden">
+                                  <span style="margin: 15px"> Total Amount : <b><span id="total_amount_show"> 0</span></b></span>
                                 </div> 
                               </div>
                             </div> 
@@ -82,10 +82,16 @@
    <script>
      var totalPoints = 0;
     function amontAdd() {
-        
-         adult_div = $('#adult_div').val()
-         children_div = $('#children_div').val() 
-          totalPoints += adult_div 
+         adult_div = parseInt($('#adult_div').val());
+         adult_div = adult_div * 100;
+
+         children_div = parseInt($('#children_div').val());
+         children_div = children_div * 50
+         totalPoints = adult_div+children_div 
+
+        $('#total_amount_show').html(totalPoints)
+        $('#total_amount_hidden').val(totalPoints)
+          console.log(totalPoints)
     }
        
    </script>
