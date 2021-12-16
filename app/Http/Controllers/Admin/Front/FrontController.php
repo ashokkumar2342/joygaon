f01f7692b79971b27d7dc11707cf530ccb55ec9f
ashokkumar2342 <ashokkumar2342@gmail.com>
2021-12-16 14:27:04 +0530

    Update FrontController.php

--- app/Http/Controllers/Admin/Front/FrontController.php
+++ app/Http/Controllers/Admin/Front/FrontController.php
@@ -165,7 +165,7 @@ class FrontController extends Controller
         }
         if (!empty($request->type)) { 
             $booking_type=DB::select(DB::raw("SELECT * FROM `bivents_booking_type` where `id` =$request->booking_type LIMIT 1"));
-            $trip_date='31-12-2021';
+            
             $ad_amount=0;
             $ch_amount=0;
             $ticket_rate_adult=$booking_type[0]->package_price;
@@ -175,7 +175,7 @@ class FrontController extends Controller
 
             $order_id = uniqid();
             
-            DB::select(DB::raw("INSERT Into `booking` (`user_id`, `booking_type_id`, `booking_date`, `trip_date`,`adults`, `children`, `person_name`, `mobile_no`, `email_id`, `ticket_rate_adult`, `ticket_rate_child`, `amount`, `status`, `remarks`, `order_id` ,`url_type`) Values ($user_id, '$request->booking_type', '$booking_date', '$trip_date','0', '0','$contact_name','$request->contact_mobile_no','$request->email_id','$ad_amount','$ch_amount','$total_amount', 0, 'Bivents', '$order_id',1);"));
+            DB::select(DB::raw("INSERT Into `booking` (`user_id`, `booking_type_id`, `booking_date`, `trip_date`,`adults`, `children`, `person_name`, `mobile_no`, `email_id`, `ticket_rate_adult`, `ticket_rate_child`, `amount`, `status`, `remarks`, `order_id` ,`url_type`) Values ($user_id, '$request->booking_type', '$booking_date', '2021-12-30','0', '0','$contact_name','$request->contact_mobile_no','$request->email_id','$ad_amount','$ch_amount','$total_amount', 0, 'Bivents', '$order_id',1);"));
 
         }
         $booking_id=DB::select(DB::raw("SELECT `id` FROM `booking` where `user_id` = $user_id and `order_id` = '$order_id' ORDER BY `id` DESC LIMIT 1")); 
